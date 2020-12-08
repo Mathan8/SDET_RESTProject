a9a6e686511c94617fc0d23746df828980c70f79
Mathan8 <mathan.ibt@gmail.com>
2020-12-08 17:16:04 +0530

    SDETRESTProject

---
+++ src/main/java/SDET_Batch/App.java
@@ -0,0 +1,13 @@
+package SDET_Batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
